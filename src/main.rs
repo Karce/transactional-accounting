72a4cd0a1613c57b123a0282fc2cc9e7cc04fad8
Keaton Bruce <akbruce@protonmail.ch>
2021-10-25 17:53:58 -0400

    Move test files into their own folder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
 use csv::{ReaderBuilder, Trim};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::env;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -19,25 +19,32 @@ fn main() -> Result<()> {
         eprintln!("Please provide a CSV file to process transactions.");
     }
 
+    process_input(&args[1])?;
+
+    Ok(())
+}
+
+fn process_input(csv_path: &str) -> Result<()> {
     let mut rdr = ReaderBuilder::new()
         .trim(Trim::All)
-        .from_path(&args[1])
-        .with_context(|| format!("Failed to read provided file {}", args[1]))?;
+        .from_path(csv_path)
+        .with_context(|| format!("Failed to read provided file {}", csv_path))?;
 
     for result in rdr.deserialize() {
-        println!("{:?}", result);
+        //println!("{:?}", result);
+        // Handle errors here related to parsing this record.
         let transaction: Transaction = result?;
         println!("{:?}", transaction);
     }
-    //_csv()?;
+
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use super::{Transaction, process_input};
     use rand::distributions::Alphanumeric;
     use rand::prelude::*;
-    use super::Transaction;
 
     fn create_test_input() -> String {
         let mut csv_path: String = thread_rng()
@@ -45,31 +52,23 @@ mod tests {
             .take(10)
             .map(char::from)
             .collect();
+        csv_path.insert_str(0, "test-");
         csv_path += ".csv";
 
-        let num_lines: u16 = rand::random();
+        let num_lines: u8 = rand::random();
         let mut wtr = csv::Writer::from_path(&csv_path).unwrap();
-        let t_types: Vec<&str> = vec!("deposit", "withdrawal", "dispute", "resolve", "chargeback");
+        let t_types: Vec<&str> = vec!["deposit", "withdrawal", "dispute", "resolve", "chargeback"];
         for _i in 0..num_lines {
             for t_type in t_types.iter() {
                 let client = random::<u16>();
                 let tx = random::<u32>();
                 let amount = random::<f32>();
                 wtr.serialize(Transaction {
-
                     r#type: t_type.to_string(),
                     client,
                     tx,
-                    amount
+                    amount,
                 })
-                /*
-                wtr.write_record(&[
-                    t_type,
-                    &client,
-                    &tx,
-                    &amount,
-                ])
-                */
                 .unwrap();
             }
         }
@@ -79,7 +78,9 @@ mod tests {
 
     #[test]
     fn test_process_input() {
-        println!("{}", create_test_input());
+        let test_file: String = create_test_input();
+        println!("{}", &test_file);
+        process_input(&test_file).unwrap();
         assert!(false);
     }
 }
