d43d521985b586b82289e003746c84849ebef10c
Keaton Bruce <akbruce@protonmail.ch>
2021-10-25 21:24:59 -0400

    Replace state with accounts

--- src/main.rs
+++ src/main.rs
@@ -63,31 +63,31 @@ fn process_input(csv_path: &str) -> Result<HashMap<u16, Account>> {
         .from_path(csv_path)
         .with_context(|| format!("Failed to read provided file {}", csv_path))?;
 
-    let mut state: HashMap<u16, Account> = HashMap::new();
+    let mut accounts: HashMap<u16, Account> = HashMap::new();
     let mut transactions: HashMap<u32, Transaction> = HashMap::new();
 
     for result in rdr.deserialize() {
         let transaction: Transaction = result?;
         // Handle the case of locked accounts after a chargeback.
-        if let Some(account) = state.get(&transaction.client) {
+        if let Some(account) = accounts.get(&transaction.client) {
             if account.locked {
                 continue;
             }
         }
-        process_transaction(&mut state, transaction, &mut transactions);
+        process_transaction(&mut accounts, transaction, &mut transactions);
     }
 
     let mut wtr = csv::Writer::from_writer(io::stdout());
 
-    for (_, account) in &state {
+    for (_, account) in &accounts {
         wtr.serialize(account)?;
     }
 
-    Ok(state)
+    Ok(accounts)
 }
 
 fn process_transaction(
-    state: &mut HashMap<u16, Account>,
+    accounts: &mut HashMap<u16, Account>,
     mut transaction: Transaction,
     transactions: &mut HashMap<u32, Transaction>,
 ) {
@@ -100,8 +100,8 @@ fn process_transaction(
     }
     match transaction.r#type.as_str() {
         "deposit" => {
-            if !state.contains_key(&transaction.client) {
-                state.insert(
+            if !accounts.contains_key(&transaction.client) {
+                accounts.insert(
                     transaction.client,
                     Account {
                         client: transaction.client,
@@ -112,7 +112,7 @@ fn process_transaction(
                     },
                 );
             } else {
-                let existing = state.get(&transaction.client).unwrap();
+                let existing = accounts.get(&transaction.client).unwrap();
                 let new_account = Account {
                     client: transaction.client,
                     available: existing.available + transaction.amount.unwrap(),
@@ -120,16 +120,16 @@ fn process_transaction(
                     total: existing.total + transaction.amount.unwrap(),
                     locked: existing.locked,
                 };
-                state.insert(transaction.client, new_account);
+                accounts.insert(transaction.client, new_account);
             }
             transaction.disputed = Some(false);
             transactions.insert(transaction.tx, transaction);
         }
         "withdrawal" => {
-            if !state.contains_key(&transaction.client) {
+            if !accounts.contains_key(&transaction.client) {
                 // No held account. Denied.
             } else {
-                let existing = state.get(&transaction.client).unwrap();
+                let existing = accounts.get(&transaction.client).unwrap();
                 if existing.available < transaction.amount.unwrap() {
                     // Insufficient funds. Denied.
                 } else {
@@ -140,19 +140,19 @@ fn process_transaction(
                         total: existing.total - transaction.amount.unwrap(),
                         locked: existing.locked,
                     };
-                    state.insert(transaction.client, new_account);
+                    accounts.insert(transaction.client, new_account);
                 }
             }
             transaction.disputed = Some(false);
             transactions.insert(transaction.tx, transaction);
         }
         "dispute" => {
-            if !state.contains_key(&transaction.client)
+            if !accounts.contains_key(&transaction.client)
                 || !transactions.contains_key(&transaction.tx)
             {
                 // No held account. Denied.
             } else {
-                let existing = state.get(&transaction.client).unwrap();
+                let existing = accounts.get(&transaction.client).unwrap();
                 let old_transaction = transactions.get_mut(&transaction.tx).unwrap();
                 old_transaction.disputed = Some(true);
                 let mut available: f32 = existing.available;
@@ -175,16 +175,16 @@ fn process_transaction(
                     total: existing.total,
                     locked: existing.locked,
                 };
-                state.insert(transaction.client, new_account);
+                accounts.insert(transaction.client, new_account);
             }
         }
         "resolve" => {
-            if !state.contains_key(&transaction.client)
+            if !accounts.contains_key(&transaction.client)
                 || !transactions.contains_key(&transaction.tx)
             {
                 // No held account. Denied.
             } else {
-                let existing = state.get(&transaction.client).unwrap();
+                let existing = accounts.get(&transaction.client).unwrap();
                 let old_transaction = transactions.get_mut(&transaction.tx).unwrap();
                 match old_transaction.disputed {
                     Some(flag) => {
@@ -197,7 +197,7 @@ fn process_transaction(
                                 total: existing.total,
                                 locked: existing.locked,
                             };
-                            state.insert(transaction.client, new_account);
+                            accounts.insert(transaction.client, new_account);
                         }
                     }
                     None => {}
@@ -205,12 +205,12 @@ fn process_transaction(
             }
         }
         "chargeback" => {
-            if !state.contains_key(&transaction.client)
+            if !accounts.contains_key(&transaction.client)
                 || !transactions.contains_key(&transaction.tx)
             {
                 // No held account. Denied.
             } else {
-                let existing = state.get(&transaction.client).unwrap();
+                let existing = accounts.get(&transaction.client).unwrap();
                 let old_transaction = transactions.get(&transaction.tx).unwrap();
                 match old_transaction.disputed {
                     Some(flag) => {
@@ -222,7 +222,7 @@ fn process_transaction(
                                 total: existing.total - old_transaction.amount.unwrap(),
                                 locked: true,
                             };
-                            state.insert(transaction.client, new_account);
+                            accounts.insert(transaction.client, new_account);
                         }
                     }
                     None => {}
@@ -239,76 +239,76 @@ mod tests {
 
     #[test]
     fn test_deposit_regular_accounts() {
-        let state = process_input("test_csvs/deposit1.csv").unwrap();
-        assert_eq!(state.get(&1).unwrap().available, 3.0);
-        assert_eq!(state.get(&1).unwrap().total, 3.0);
+        let accounts = process_input("test_csvs/deposit1.csv").unwrap();
+        assert_eq!(accounts.get(&1).unwrap().available, 3.0);
+        assert_eq!(accounts.get(&1).unwrap().total, 3.0);
 
-        assert_eq!(state.get(&2).unwrap().available, 2.0);
-        assert_eq!(state.get(&2).unwrap().total, 2.0);
+        assert_eq!(accounts.get(&2).unwrap().available, 2.0);
+        assert_eq!(accounts.get(&2).unwrap().total, 2.0);
 
-        assert_eq!(state.get(&3).unwrap().available, 2.0);
-        assert_eq!(state.get(&3).unwrap().total, 2.0);
+        assert_eq!(accounts.get(&3).unwrap().available, 2.0);
+        assert_eq!(accounts.get(&3).unwrap().total, 2.0);
 
-        assert_eq!(state.get(&999).unwrap().available, 2.0567);
-        assert_eq!(state.get(&999).unwrap().total, 2.0567);
+        assert_eq!(accounts.get(&999).unwrap().available, 2.0567);
+        assert_eq!(accounts.get(&999).unwrap().total, 2.0567);
     }
 
     #[test]
     fn test_withdraw_regular_accounts() {
-        let state = process_input("test_csvs/withdraw1.csv").unwrap();
-        assert_eq!(state.get(&1).unwrap().available, 1.5);
-        assert_eq!(state.get(&1).unwrap().total, 1.5);
+        let accounts = process_input("test_csvs/withdraw1.csv").unwrap();
+        assert_eq!(accounts.get(&1).unwrap().available, 1.5);
+        assert_eq!(accounts.get(&1).unwrap().total, 1.5);
 
-        assert_eq!(state.get(&2).unwrap().available, 2.0);
-        assert_eq!(state.get(&2).unwrap().total, 2.0);
+        assert_eq!(accounts.get(&2).unwrap().available, 2.0);
+        assert_eq!(accounts.get(&2).unwrap().total, 2.0);
 
-        assert!(!state.contains_key(&3));
+        assert!(!accounts.contains_key(&3));
     }
 
     #[test]
     fn test_dispute_regular_deposit() {
-        let state = process_input("test_csvs/dispute1.csv").unwrap();
-        assert_eq!(state.get(&1).unwrap().available, 1.0);
-        assert_eq!(state.get(&1).unwrap().held, 2.5);
-        assert_eq!(state.get(&1).unwrap().total, 3.5);
-        assert_eq!(state.get(&1).unwrap().locked, false);
+        let accounts = process_input("test_csvs/dispute1.csv").unwrap();
+        assert_eq!(accounts.get(&1).unwrap().available, 1.0);
+        assert_eq!(accounts.get(&1).unwrap().held, 2.5);
+        assert_eq!(accounts.get(&1).unwrap().total, 3.5);
+        assert_eq!(accounts.get(&1).unwrap().locked, false);
     }
 
     #[test]
     fn test_dispute_regular_withdrawal() {
         // I'm defining withdrawals as undisputable since
         // performing chargebacks on a withdrawal doesn't make sense.
-        let state = process_input("test_csvs/dispute2.csv").unwrap();
-        assert_eq!(state.get(&1).unwrap().available, 2.0);
-        assert_eq!(state.get(&1).unwrap().held, 0.0);
-        assert_eq!(state.get(&1).unwrap().total, 2.0);
-        assert_eq!(state.get(&1).unwrap().locked, false);
+        let accounts = process_input("test_csvs/dispute2.csv").unwrap();
+        assert_eq!(accounts.get(&1).unwrap().available, 2.0);
+        assert_eq!(accounts.get(&1).unwrap().held, 0.0);
+        assert_eq!(accounts.get(&1).unwrap().total, 2.0);
+        assert_eq!(accounts.get(&1).unwrap().locked, false);
     }
 
     #[test]
     fn test_resolve_regular_dispute() {
-        let state = process_input("test_csvs/resolve1.csv").unwrap();
-        assert_eq!(state.get(&1).unwrap().available, 3.5);
-        assert_eq!(state.get(&1).unwrap().held, 0.0);
-        assert_eq!(state.get(&1).unwrap().total, 3.5);
-        assert_eq!(state.get(&1).unwrap().locked, false);
+        let accounts = process_input("test_csvs/resolve1.csv").unwrap();
+        assert_eq!(accounts.get(&1).unwrap().available, 3.5);
+        assert_eq!(accounts.get(&1).unwrap().held, 0.0);
+        assert_eq!(accounts.get(&1).unwrap().total, 3.5);
+        assert_eq!(accounts.get(&1).unwrap().locked, false);
     }
 
     #[test]
     fn test_chargeback_regular_dispute() {
-        let state = process_input("test_csvs/chargeback1.csv").unwrap();
-        assert_eq!(state.get(&1).unwrap().available, 1.0);
-        assert_eq!(state.get(&1).unwrap().held, 0.0);
-        assert_eq!(state.get(&1).unwrap().total, 1.0);
-        assert_eq!(state.get(&1).unwrap().locked, true);
+        let accounts = process_input("test_csvs/chargeback1.csv").unwrap();
+        assert_eq!(accounts.get(&1).unwrap().available, 1.0);
+        assert_eq!(accounts.get(&1).unwrap().held, 0.0);
+        assert_eq!(accounts.get(&1).unwrap().total, 1.0);
+        assert_eq!(accounts.get(&1).unwrap().locked, true);
     }
 
     #[test]
     fn test_chargeback_more_disputes() {
-        let state = process_input("test_csvs/chargeback2.csv").unwrap();
-        assert_eq!(state.get(&1).unwrap().available, 1.0);
-        assert_eq!(state.get(&1).unwrap().held, 0.0);
-        assert_eq!(state.get(&1).unwrap().total, 1.0);
-        assert_eq!(state.get(&1).unwrap().locked, true);
+        let accounts = process_input("test_csvs/chargeback2.csv").unwrap();
+        assert_eq!(accounts.get(&1).unwrap().available, 1.0);
+        assert_eq!(accounts.get(&1).unwrap().held, 0.0);
+        assert_eq!(accounts.get(&1).unwrap().total, 1.0);
+        assert_eq!(accounts.get(&1).unwrap().locked, true);
     }
 }
